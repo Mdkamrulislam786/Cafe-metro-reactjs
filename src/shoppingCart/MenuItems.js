7bf1d38d806e266031477c72770bfc7ef6878ac0
Mdkamrulislam786 <kamrulislam.ki01@gmail.com>
2020-10-05 01:28:55 +0600

    new products

--- src/shoppingCart/MenuItems.js
+++ src/shoppingCart/MenuItems.js
@@ -1,14 +1,18 @@
 import React from "react";
-import {Button} from 'react-bootstrap'
+import { Button } from "react-bootstrap";
 
 const MenuItems = (props) => {
   return (
     <div>
       <div className="images">
         <img className="image" src={props.src} alt={props.alt} />
-        <h3>{props.h3}</h3>
-        <h3>{props.price}</h3>
-        <Button onClick={props.onClick}  className="addToCart carts">
+        <div>
+          <h3 style={{ textAlign: "center" }}>{props.h3}</h3>
+          <h3 style={{ textAlign: "center", fontSize: "20px" }}>
+            {props.price}
+          </h3>
+        </div>
+        <Button onClick={props.onClick} className="addToCart carts">
           <i className="fas fa-shopping-cart">Add to Cart</i>
         </Button>
       </div>
